0c3e293e0229c6262a707d5833ef6f0a30a21fe9
Aminkhan1433 <aminkhansayfullayev@gmail.com>
2025-07-21 16:11:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let name = prompt ("your name")
+
+let letter = prompt ("letter")
+
+if (name.toLowerCase().includes(letter.toLowerCase())) {
+    console.log(true);
+    
+}else {
+    console.log(false);
+    
+}
+
+
+
+
+
+
+// let name = prompt("your name");
+// let letter = prompt("letter");
+
+// if (name.toLowerCase().includes(letter.toLowerCase())) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
